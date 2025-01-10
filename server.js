34de46a17e25b5bab3537215059554bec19ef3ab
zandanbatzorig <49382839+zandanbatzorig@users.noreply.github.com>
2025-01-11 05:09:30 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 // server.js
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+// Connect to MongoDB
+const MONGO_URI = 'mongodb://127.0.0.1:27017/testdb4';
+mongoose.connect(MONGO_URI, {})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 // Serve the HTML file
 app.get('/', (req, res) => {
